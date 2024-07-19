1dc2918050b76cedb2441241f08dfd9120659567
reinhardtt48 <reinhardttyler2@gmail.com>
2024-07-18 20:15:54 -0500

    Health & Stamina Bar and 1st Attack

--- WolfFighter/scripts/player.gd
+++ WolfFighter/scripts/player.gd
@@ -3,57 +3,84 @@ extends CharacterBody2D
 @export var SPEED = 200
 @export var health = 100
 @export var type = "Human"
-@export var STAMINA: float = 10
-var stamina: float = STAMINA
+@export var MAX_STAMINA: float = 10
+var cur_stamina: float = MAX_STAMINA
 
 var enemyInRange = false
 var enemyCooldown = true
 var playerAlive = true
 
-func player():
-	pass
+@onready var stamina_bar = get_node("Stamina_Bar")
+
+
 
 func _ready():
 	$AnimatedSprite2D.play("frontidle")
 
 func move_player():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
-	if Input.is_action_pressed("right"):
-		$AnimatedSprite2D.play("rightwalk")
-	if Input.is_action_just_released("right"):
-		$AnimatedSprite2D.play("rightidle")
-	if Input.is_action_pressed("left"):
-		$AnimatedSprite2D.play("leftwalk")
-	if Input.is_action_just_released("left"):
-		$AnimatedSprite2D.play("leftidle")
-	if Input.is_action_pressed("down"):
-		$AnimatedSprite2D.play("frontwalk")
-	if Input.is_action_just_released("down"):
-		$AnimatedSprite2D.play("frontidle")
-	if Input.is_action_pressed("up"):
-		$AnimatedSprite2D.play("upwalk")
-	if Input.is_action_just_released("up"):
-		$AnimatedSprite2D.play("upidle")
-	if Input.is_action_pressed("special"): 
-		if stamina > 0:
-			stamina -= 1
-			velocity *= 3
-	if !Input.is_action_pressed("special"):
-		while stamina < STAMINA:
-			stamina += 0.001
+	if playerAlive == true:
+		var input_direction = Input.get_vector("left", "right", "up", "down")
+		velocity = input_direction * SPEED
+		if Input.is_action_pressed("right"):
+			$AnimatedSprite2D.play("rightwalk")
+			$meele_attack_area.rotation_degrees = -90
+		if Input.is_action_just_released("right"):
+			$AnimatedSprite2D.play("rightidle")
+			
+		if Input.is_action_pressed("left"):
+			$AnimatedSprite2D.play("leftwalk")
+			$meele_attack_area.rotation_degrees = 90
+		if Input.is_action_just_released("left"):
+			$AnimatedSprite2D.play("leftidle")
+			
+		if Input.is_action_pressed("down"):
+			$AnimatedSprite2D.play("frontwalk")
+			$meele_attack_area.rotation_degrees = 0
+		if Input.is_action_just_released("down"):
+			$AnimatedSprite2D.play("frontidle")
+			
+		if Input.is_action_pressed("up"):
+			$AnimatedSprite2D.play("upwalk")
+			$meele_attack_area.rotation_degrees = 180
+		if Input.is_action_just_released("up"):
+			$AnimatedSprite2D.play("upidle")
+			
+		if Input.is_action_pressed("special"): 
+			if cur_stamina > 0:
+				cur_stamina -= 1
+				#TextureProgressBar
+				stamina_bar.value -= 1
+				velocity *= 3
+		if !Input.is_action_pressed("special"):
+			while cur_stamina < MAX_STAMINA:
+				cur_stamina += 0.001
+				stamina_bar.value += 0.001
+				
+		if Input.is_action_pressed("attack_1"):
+#			$meele_attack_area/cut_collision_box.disabled = false
+#			$meele_attack_area/Cut_animation.visible = true
+			player_attack()
+		if Input.is_action_just_released("attack_1"):
+#			$meele_attack_area/cut_collision_box.disabled = true
+#			$meele_attack_area/Cut_animation.visible = false
+			player_attack_reset()
+			pass
 
 func _physics_process(delta):
 	move_player()
-	#print(health)
+	#print("Player: " + str(health))
 	enemy_attack()
 	move_and_slide()
 	
-	if health <= 0:
+	if health <= 0 && playerAlive == true:
 		playerAlive = false
 		health = 0
-		print("dead")
+		$AnimatedSprite2D.play("Dead")
+		print("Player: dead")
 		#add game over here
+
+func player():
+	pass
 
 func _on_player_hitbox_body_entered(body):
 	if body.has_method("enemy"):
@@ -64,11 +91,39 @@ func _on_player_hitbox_body_exited(body):
 		enemyInRange = false
 
 func enemy_attack():
-	if enemyInRange and enemyCooldown == true:
+	if enemyInRange and enemyCooldown == true && playerAlive == true:
 		health = health - 20
+		$Health_Bar.value -= 20
 		enemyCooldown = false
-		$attackCooldown.start()
-		print(health)
+		$enemyAttackCooldown.start()
+		print("Player: " + str(health))
 
 func _on_attack_cooldown_timeout():
 	enemyCooldown = true
+
+func player_attack():
+	$meele_attack_area/cut_collision_box.disabled = false
+	$meele_attack_area/Cut_animation.visible = true
+	global.playerIsAttacking = true
+#	for i in 4:
+#		$meele_attack_area.position = Vector2i(i,0)
+#	$meele_attack_area/cut_collision_box.disabled = true
+#	$meele_attack_area/Cut_animation.visible = false
+#	$meele_attack_area.position = Vector2i(0,0)
+	pass
+	
+func player_attack_reset():
+	$meele_attack_area/cut_collision_box.disabled = true
+	$meele_attack_area/Cut_animation.visible = false
+	$meele_attack_area.position = Vector2i(0,0)
+	global.playerIsAttacking = false
+	pass
+
+#func _on_player_attack_cooldown_timeout():
+#	pass # Replace with function body.
+#
+#
+#func _on_meele_attack_area_area_entered(area):
+##	if area.is_in_group("hurtbox")
+##		area.takeDamage()
+#	pass # Replace with function body.
--- WolfFighter/scripts/wolf.gd
+++ WolfFighter/scripts/wolf.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 
 var player_chase=false
 var playerAttackRange = false
+var player_cooldown = true
 var player = null
 var time=0.0
 
@@ -46,17 +47,47 @@ func enemy():
 	pass
 
 func _on_hit_range_body_entered(body):
+#	print(body)
 	if body.has_method("player"):
 		playerAttackRange = true
+		print("Player in range")
 
 
 func _on_hit_range_body_exited(body):
 	if body.has_method("player"):
 		playerAttackRange = false
+		print("player out of range")
 
 func check_damage():
-	if playerAttackRange and global.playerIsAttacking == true:
+#	print("Player in Range: ", playerAttackRange)
+#	print("Player is Attaking: ", global.playerIsAttacking)
+#	print("cooldown: ", player_cooldown)
+#	print()
+#						
+	if playerAttackRange and global.playerIsAttacking == true && player_cooldown == true:
 		health -= 20
-		print("Wolf Health =", health)
+		$Health_Bar.value -= 20
+		print("Wolf Health = ", health)
 		if health <= 0:
 			self.queue_free()
+			print("Wolf Dead")
+			pass
+		player_cooldown == false
+
+
+func _on_hurt_range_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_hurt_range_body_exited(body):
+	pass # Replace with function body.
+
+
+#func _on_player_attack_cooldown_timeout():
+#
+#	pass # Replace with function body.
+
+
+func _on_player_attack_cooldown_timeout():
+	player_cooldown = true
+	pass # Replace with function body.
